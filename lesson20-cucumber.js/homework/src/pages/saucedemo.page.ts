3733038c8077afa327a65a0746877916c6e4f745
Worker <hirgoff@gmail.com>
2025-12-06 15:05:29 +0200

    lesson20-hw-fixed

--- lesson20-cucumber.js/homework/src/pages/saucedemo.page.ts
+++ lesson20-cucumber.js/homework/src/pages/saucedemo.page.ts
@@ -1,14 +1,37 @@
 import { Locator, Page } from 'playwright';
 
 export class SauceDemoPage {
-    public get usernameInput(): Locator { return this.page.locator('[data-test="username"]'); }
-    public get passwordInput(): Locator { return this.page.locator('[data-test="password"]'); }
-    public get loginButton(): Locator { return this.page.locator('[data-test="login-button"]'); }
-    public get errorMessage(): Locator { return this.page.locator('[data-test="error"]'); }
-    public get pageTitle(): Locator { return this.page.locator('.title'); }
-    public get cartBadge(): Locator { return this.page.locator('.shopping_cart_badge'); }
-    public get cartLink(): Locator { return this.page.locator('.shopping_cart_link'); }
-    public get cartItems(): Locator { return this.page.locator('.cart_item'); }
+    private get usernameInput(): Locator {
+        return this.page.locator('[data-test="username"]');
+    }
+
+    private get passwordInput(): Locator {
+        return this.page.locator('[data-test="password"]');
+    }
+
+    private get loginButton(): Locator {
+        return this.page.locator('[data-test="login-button"]');
+    }
+
+    private get errorMessage(): Locator {
+        return this.page.locator('[data-test="error"]');
+    }
+
+    private get pageTitle(): Locator {
+        return this.page.locator('.title');
+    }
+
+    private get cartBadge(): Locator {
+        return this.page.locator('.shopping_cart_badge');
+    }
+
+    private get cartLink(): Locator {
+        return this.page.locator('.shopping_cart_link');
+    }
+
+    private get cartItems(): Locator {
+        return this.page.locator('.cart_item');
+    }
 
     public constructor(private readonly page: Page) {}
 
@@ -22,12 +45,46 @@ export class SauceDemoPage {
         await this.loginButton.click();
     }
 
-    public getInventoryItem(name: string): Locator {
-        return this.page.locator('.inventory_item', { hasText: name });
+    public async isErrorVisible(): Promise<boolean> {
+        return await this.errorMessage.isVisible();
     }
 
-    public getCartItem(name: string): Locator {
-        return this.page.locator('.cart_item', { hasText: name });
+    public async getErrorText(): Promise<string> {
+        return (await this.errorMessage.textContent()) ?? '';
+    }
+
+    public async waitForInventoryPage(): Promise<void> {
+        await this.pageTitle.waitFor({ state: 'visible' });
+    }
+
+    public async getPageTitle(): Promise<string> {
+        return (await this.pageTitle.textContent()) ?? '';
+    }
+
+    public async addItemToCart(itemName: string): Promise<void> {
+        const item = this.page.locator('.inventory_item', { hasText: itemName });
+        await item.locator('button[data-test^="add-to-cart"]').click();
+    }
+
+    public async getCartBadgeCount(): Promise<string> {
+        return (await this.cartBadge.textContent()) ?? '0';
+    }
+
+    public async goToCart(): Promise<void> {
+        await this.cartLink.click();
+    }
+
+    public async isItemInCart(itemName: string): Promise<boolean> {
+        const item = this.page.locator('.cart_item', { hasText: itemName });
+        return await item.isVisible();
+    }
+
+    public async removeItemFromCart(itemName: string): Promise<void> {
+        const item = this.page.locator('.cart_item', { hasText: itemName });
+        await item.locator('button[data-test^="remove"]').click();
+    }
+
+    public async getCartItemsCount(): Promise<number> {
+        return await this.cartItems.count();
     }
 }
-
--- lesson20-cucumber.js/homework/src/steps/saucedemo.steps.ts
+++ lesson20-cucumber.js/homework/src/steps/saucedemo.steps.ts
@@ -11,42 +11,42 @@ When('the user logs in with username {string} and password {string}', async func
 });
 
 Then('an error message should be displayed', async function (this: SauceDemoWorld) {
-    expect(await this.sauceDemoPage.errorMessage.isVisible()).to.be.true;
+    expect(await this.sauceDemoPage.isErrorVisible()).to.be.true;
 });
 
 Then('the error message should contain {string}', async function (this: SauceDemoWorld, text: string) {
-    const message = await this.sauceDemoPage.errorMessage.textContent();
+    const message = await this.sauceDemoPage.getErrorText();
     expect(message).to.include(text);
 });
 
 Then('the user should be redirected to the inventory page', async function (this: SauceDemoWorld) {
-    await this.sauceDemoPage.pageTitle.waitFor({ state: 'visible' });
+    await this.sauceDemoPage.waitForInventoryPage();
 });
 
 Then('the page title should be {string}', async function (this: SauceDemoWorld, title: string) {
-    expect(await this.sauceDemoPage.pageTitle.textContent()).to.equal(title);
+    expect(await this.sauceDemoPage.getPageTitle()).to.equal(title);
 });
 
 When('the user adds {string} to the cart', async function (this: SauceDemoWorld, itemName: string) {
-    await this.sauceDemoPage.getInventoryItem(itemName).locator('button[data-test^="add-to-cart"]').click();
+    await this.sauceDemoPage.addItemToCart(itemName);
 });
 
 Then('the cart badge should show {string} item', async function (this: SauceDemoWorld, count: string) {
-    expect(await this.sauceDemoPage.cartBadge.textContent()).to.equal(count);
+    expect(await this.sauceDemoPage.getCartBadgeCount()).to.equal(count);
 });
 
 When('the user goes to the cart', async function (this: SauceDemoWorld) {
-    await this.sauceDemoPage.cartLink.click();
+    await this.sauceDemoPage.goToCart();
 });
 
 Then('the cart should contain {string}', async function (this: SauceDemoWorld, itemName: string) {
-    expect(await this.sauceDemoPage.getCartItem(itemName).isVisible()).to.be.true;
+    expect(await this.sauceDemoPage.isItemInCart(itemName)).to.be.true;
 });
 
 When('the user removes {string} from the cart', async function (this: SauceDemoWorld, itemName: string) {
-    await this.sauceDemoPage.getCartItem(itemName).locator('button[data-test^="remove"]').click();
+    await this.sauceDemoPage.removeItemFromCart(itemName);
 });
 
 Then('the cart should be empty', async function (this: SauceDemoWorld) {
-    expect(await this.sauceDemoPage.cartItems.count()).to.equal(0);
+    expect(await this.sauceDemoPage.getCartItemsCount()).to.equal(0);
 });
